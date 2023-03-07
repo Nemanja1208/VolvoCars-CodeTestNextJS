66d85924121b5100ab909494ba3b03e427dcf5b6
MananaNemanja <nemanja.milosavljevic@skf.com>
2023-03-07 23:35:24 +0100

    worksWithPrettierAndNextJS13

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage: React.FC = () => {
+  return (<h1>Hello World!</h1>);
+};
+
+export const metadata = {
+    title: 'Volvo Cars Coding Test',
+  };
+
+export default Homepage;
